063b6d7abf052e9946ce58465297cc13db815568
dimi9595 <dmitry.mindlin@gmail.com>
2024-10-01 15:32:46 +0200

    Corrected var name.

--- src/routes/exit/+page.svelte
+++ src/routes/exit/+page.svelte
@@ -14,6 +14,6 @@
 
 <div class="col-span-3 space-y-4 p-6 sm:p-8 md:space-y-6 w-3/4 mx-auto">
     {#if user_id}
-        <ExitQuestionnaire {user_id} {studyGroup}/>
+        <ExitQuestionnaire {user_id} {study_group}/>
     {/if}
 </div>
